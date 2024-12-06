fcd9b64277c9ddc4531777c6ade76829e9eb14f6
김민석 <skyfox001015@naver.com>
2024-12-07 00:29:40 +0900

    Terminal fix : roomThread가 1분에 한번씩 방에 유저가 있는지 확인  ef779e

--- src/main/java/server/RoomThread.java
+++ src/main/java/server/RoomThread.java
@@ -1,16 +1,16 @@
 package server;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import server.manager.RoomManager;
 import server.model.Room;
 
 public class RoomThread implements Runnable {
 
-    private final BlockingQueue<Runnable> taskQueue = new LinkedBlockingQueue<>();
     private final Room room;
     private volatile boolean running = true;
+    private final RoomManager roomManager;
 
-    public RoomThread(Room room) {
+    public RoomThread(Room room, RoomManager roomManager) {
+        this.roomManager = roomManager;
         this.room = room;
     }
 
@@ -18,21 +18,31 @@ public class RoomThread implements Runnable {
     public void run() {
         while (running) {
             try {
-                Runnable task = taskQueue.take();
-                task.run();
+                // 방에 유저가 있는지 확인
+                checkRoomStatus();
+
+                // 1분마다 확인 (60초 대기)
+                Thread.sleep(60 * 1000); // 1분 대기
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
         }
     }
 
-    public void addTask(Runnable task) {
-        taskQueue.offer(task);
+    /**
+     * 방에 유저가 있는지 확인하고, 없으면 쓰레드를 종료.
+     */
+    private void checkRoomStatus() {
+        if (room.getCurrentPlayers() == 0) {
+            System.out.println("방 ID: " + room.getRoomId() + "에 사용자가 없습니다. 쓰레드를 종료합니다.");
+            roomManager.deleteRoom(room.getRoomId());
+        }
     }
 
+    /**
+     * 방 쓰레드 종료
+     */
     public void stopThread() {
         running = false;
-        taskQueue.offer(() -> {
-        }); // 큐를 깨워서 쓰레드 종료
     }
 }
